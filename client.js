b53eff573bab348e12319baa3e7adf72ea95dbe8
EPIIC - L <contact@epiic.fr>
2018-02-12 17:36:45 +0100

    add node simple-git

--- client.js
+++ client.js
@@ -1,20 +1,37 @@
-var socket   = require('socket.io-client')('https://shielded-dawn-50249.herokuapp.com/');
-var notifier = require('node-notifier');
-const path   = require('path');
+var socket      = require('socket.io-client')('https://shielded-dawn-50249.herokuapp.com/');
+var notifier    = require('node-notifier');
+const path      = require('path');
+var projectPath = path.resolve(__dirname);
+const git       = require('simple-git')(projectPath);
 
 //Lors de la réception de la clé "notification"
 socket.on('notification', function(data){
 
+    //Si le paramètre title n'existe pas, on le définit automatiquement
     if(!data.title) { data.title = 'EPIIC - notification interne'; }
 
-    notifier.notify(
-        {
-            title: data.title,
-            message: data.message,
-            icon: path.join(__dirname, 'logo_epiic.png'), // Absolute path (doesn't work on balloons)
-            sound: true, // Only Notification Center or Windows Toasters
-            wait: false // Wait with callback, until user action is taken against notification
-        },
-        function(err, response) {}
-    );
+    //Si on catch la clé &update dans le message
+    if(data.message == '&update') {
+
+        notifier.notify({
+            title   : data.title,
+            message : 'Lancement du procéssus de mise à jours automatique',
+            icon    : path.join(__dirname, 'logo_epiic.png'), // Absolute path (doesn't work on balloons)
+            sound   : true, // Only Notification Center or Windows Toasters
+            wait    : false // Wait with callback, until user action is taken against notification
+        });
+
+
+        git.pull('origin', 'master');
+    }
+    else {
+
+        notifier.notify({
+            title   : data.title,
+            message : data.message,
+            icon    : path.join(__dirname, 'logo_epiic.png'), // Absolute path (doesn't work on balloons)
+            sound   : true, // Only Notification Center or Windows Toasters
+            wait    : false // Wait with callback, until user action is taken against notification
+        });
+    }
 });
